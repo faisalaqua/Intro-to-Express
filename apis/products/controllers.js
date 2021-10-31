8c48bd31ea08b6236ccbba60162064e1e9d5b27a
Your Name <you@example.com>
2021-10-31 17:46:05 +0300

    completed task

--- apis/products/controllers.js
+++ apis/products/controllers.js
@@ -2,6 +2,15 @@
 // let products = require("../../data");
 const Product = require("../../db/Models/Product");
 
+exports.fetchProduct = async (productId, next) => {
+  try {
+    const product = await Product.findById(productId);
+    return product;
+  } catch (error) {
+    next(error);
+  }
+};
+
 exports.productList = async (req, res, next) => {
   try {
     const products = await Product.find();
@@ -22,37 +31,42 @@ exports.productCreate = async (req, res, next) => {
 
 exports.productDelete = async (req, res, next) => {
   try {
-    const { productId } = req.params;
-    const productDelete = Product.findById(productId);
-    if (productDelete) {
-      await Product.remove(productDelete);
-      return res.status(204).end();
-    } else {
-      next({
-        status: 404,
-        message: "Product not found!",
-      });
-    }
-  } catch (error) {
+    await req.product.remove();
+    return res.status(204).end();
+  } catch (err) {
     next(error);
   }
 };
 
+// exports.productUpdate = async (req, res, next) => {
+//   try {
+//     // const productId = req.params.productId;
+//     // const product = await Product.findById(productId);
+//     const product = await Product.findByIdAndUpdate(
+//       { _id: productId },
+//       req.body,
+//       { new: true }
+//     );
+//     // if (product) {
+//       return res.status(200).json(product)
+//     // } else {
+//     //   return res.status(404).json({ message: "Product not found" });
+//     // }
+//   // } catch (error) {
+//   //   next(error);
+//   // }
+//     }
+//   }
+
 exports.productUpdate = async (req, res, next) => {
   try {
-    const productId = req.params.productId;
-    // const product = await Product.findById(productId);
     const product = await Product.findByIdAndUpdate(
-      { _id: productId },
+      { _id: req.product.id },
       req.body,
-      { new: true }
+      { new: true, runValidators: true } // returns the updated product
     );
-    if (product) {
-      return res.json(product).end();
-    } else {
-      return res.status(404).json({ message: "Product not found" });
-    }
-  } catch (error) {
+    res.json(product);
+  } catch (err) {
     next(error);
   }
 };
--- apis/products/routes.js
+++ apis/products/routes.js
@@ -5,7 +5,18 @@ const {
   productCreate,
   productDelete,
   productUpdate,
+  fetchProduct,
 } = require("./controllers");
+
+router.param("productId", async (req, res, next, productId) => {
+  const product = await fetchProduct(productId, next);
+  if (product) {
+    req.product = product;
+    next();
+  } else {
+    next({ status: 404, message: "Product Not Found!" });
+  }
+});
 
 router.get("/", productList);
 
